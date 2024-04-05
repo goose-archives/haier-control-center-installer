9503d55372ef4c5e09196141c82978dc90b98f17
gooseprjkt <162053558+gooseprjkt@users.noreply.github.com>
2024-04-05 18:54:04 +0300

    Create bootstrap.bat

---
+++ bootstrap.bat
@@ -0,0 +1,3 @@
+wget -O HCC.appxbundle http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/e3c2067b-17cf-4acd-95e3-7cead23c7251?P1=1712332460&P2=404&P3=2&P4=IzTfQnZAYRcnltl53DhTVyQQ1ldYyqQzQS9cTM1LK8Imo70Ub%2bCHAX8%2f6qrV42mLLSWIxnuHwiBodczKa0yucw%3d%3d
+powershell Add-AppxPackage -Path HCC.appxbundle
+echo DONE
